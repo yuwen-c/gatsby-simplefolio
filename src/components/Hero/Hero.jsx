48e5cc356aecb87ef7ff77c7869954a5ac651e17
yuwen-c <yuwen.azulejos@gmail.com>
2021-02-25 15:59:34 +0800

    use loading to replace Hi, my name is...

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -22,16 +22,22 @@ const Header = () => {
   }, []);
 
   return (
+    // !title ? <h1>loading</h1> :
     <section id="hero" className="jumbotron">
       <Container>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
           <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
+            {/* {title || 'Hi, my name is'}{' '}
             <span className="text-color-main">{name || 'Your Name'}</span>
             <span> | </span>
             <span className="text-color-main">{secondName || 'Your second name'}</span>
             <br />
-            <span style={{marginLeft:'1em'}}>{subtitle || "I'm the Unknown Developer."}</span>
+            <span style={{marginLeft:'1em'}}>{subtitle || "I'm the Unknown Developer."}</span>  */}
+            {title || 'Loading..'} <span className="text-color-main">{name}</span>
+            <span> | </span>
+            <span className="text-color-main">{secondName}</span>
+            <br />
+            <span style={{ marginLeft: '1em' }}>{subtitle}</span>
           </h1>
         </Fade>
         <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
